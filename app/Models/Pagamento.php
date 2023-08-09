ff63415199ad60475e563b1a8f789a9fe013e3d8
Julio Cesar <jccintr@gmail.com>
2023-08-08 21:40:25 -0300

    alterado models taxa e pagamento

--- app/Models/Pagamento.php
+++ app/Models/Pagamento.php
@@ -10,6 +10,7 @@ class Pagamento extends Model
     use HasFactory;
     protected $table = 'pagamentos';
     protected $fillable = ['user_id','nome','ativo'];
+    protected $casts = ['ativo'=> 'boolean'];
 
     protected $hidden = [
         'created_at',
--- app/Models/Taxa.php
+++ app/Models/Taxa.php
@@ -10,6 +10,7 @@ class Taxa extends Model
     use HasFactory;
     protected $table = 'taxas';
     protected $fillable = ['user_id','bairro','valor','ativo'];
+    protected $casts = ['ativo'=> 'boolean'];
 
     protected $hidden = [
         'created_at',
